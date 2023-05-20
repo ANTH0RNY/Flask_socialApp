6ba2049c3b7ee128dd333cd9f5495b11e28be0f4
ANTH0RNY <nyaganthony@gmail.com>
2023-05-20 20:44:30 +0300

    Done the register login and register is done

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from ..models import User
 
 class login_Form(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1,64), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me', default=True)
     submit = SubmitField('Login')
+
+class register_Form(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1,64), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(1,64)])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('conf_password', message='Password must match')])
+    conf_password = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, flash, request, url_for, redirect
 from flask_login import login_user, logout_user, login_required
 from . import auth
-from .forms import login_Form
+from .forms import login_Form, register_Form
 from ..models import User
+from .. import db
 
 @auth.route('/login', methods=['POST', 'GET'])
 def login():
@@ -29,3 +30,21 @@ def logout():
     logout_user()
     flash('You have been logged out have a nice day')
     return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['POST', 'GET'])
+def register():
+    form = register_Form()
+    if form.validate_on_submit():
+        name = form.username.data
+        email = form.email.data
+        if User.query.filter_by(username=name).first() or User.query.filter_by(email=email).first():
+            flash('Username or email is already taken')
+            return render_template("auth/register.html", form=form)
+        new_user = User(username=name, email=email)
+        new_user.password = form.password.data
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Feel free to login')
+        return redirect(url_for('auth.login'))
+            
+    return render_template("auth/register.html", form=form)
